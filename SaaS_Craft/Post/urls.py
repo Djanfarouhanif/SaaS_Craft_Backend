4d6ae4365da023530ab475e7c44dfff74622e455
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-05-01 23:09:46 +0000

    add api string for urls

--- SaaS_Craft/Post/urls.py
+++ SaaS_Craft/Post/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('settings', views.settings, name='settings'),
-    path("article_post/<str:pk>" , views.article_post, name='article_post'),
-    path("articleComment/<str:pk>", views.articleComment, name="articleComment"),
-    path("signup", views.signup, name='signup'),
-    path("signin", views.signin, name='signin'),
-    path('logout', views.logout, name='logout')
+    path('api', views.index, name='index'),
+    path('api/settings', views.settings, name='settings'),
+    path("api/article_post/<str:pk>" , views.article_post, name='article_post'),
+    path("api/articleComment/<str:pk>", views.articleComment, name="articleComment"),
+    path("api/signup", views.signup, name='signup'),
+    path("api/signin", views.signin, name='signin'),
+    path('api/logout', views.logout, name='logout')
 
     
 ]
